d53a1ad09135870087d650ae98d8fc0942950138
evgeniy1801 <evgeniy1801@gmail.com>
2021-01-29 16:22:59 +0700

    JS refactoringg

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+var MENU_ITEM_SELECTOR = '.menu__item[data-has-submenu]';
+var MENU_LINK_SELECTOR = '.menu__link';
+var MENU_OPENER_SELECTOR = '[data-menu-opener]';
+var MENU_OVERLAY_SELECTOR = '[data-menu-overlay]';
+var MENU_SELECTOR = '[data-menu]';
+var PHOTO_OPENER_SELECTOR = '[data-photo-opener]';
+var PHOTO_OVERLAY_SELECTOR = '[data-photo-overlay]';
+var PHOTO_CLOSER_SELECTOR = '[data-photo-closer]';
+var PHOTO_VIEW_SELECTOR = '[data-photo-view]';
+
+var MENU_ITEM_OPENED_CLASSNAME = 'menu__item_opened';
+var MAIN_MENU_OPENED_CLASSNAME = 'main__menu_opened';
+var PHOTO_VIEW_OPENED_CLASSNAME = 'photo-view_opened';
+
 function toggleClassName(el, className) {
   var oldClassList = el.className;
   var newClassList;
@@ -12,63 +26,63 @@ function toggleClassName(el, className) {
 }
 
 function initMenu() {
-  var menuItems = document.querySelectorAll('.menu__item[data-has-submenu]');
+  var menuItems = document.querySelectorAll(MENU_ITEM_SELECTOR);
 
   var itemCounter = 0;
   var item;
 
   while (item = menuItems[itemCounter]) {
-    item.querySelector('.menu__link').addEventListener('click', function(event) {
-      toggleClassName(event.target.parentNode, 'menu__item_opened');
+    item.querySelector(MENU_LINK_SELECTOR).addEventListener('click', function(event) {
+      toggleClassName(event.target.parentNode, MENU_ITEM_OPENED_CLASSNAME);
     });
 
     itemCounter += 1;
   }
 
-  var menuOpener = document.querySelector('[data-menu-opener]');
-  var menuOverlay = document.querySelector('[data-menu-overlay]');
-  var menu = document.querySelector('[data-menu]');
+  var menuOpener = document.querySelector(MENU_OPENER_SELECTOR);
+  var menuOverlay = document.querySelector(MENU_OVERLAY_SELECTOR);
+  var menu = document.querySelector(MENU_SELECTOR);
 
   menuOpener.addEventListener('click', function() {
-    toggleClassName(menu, 'main__menu_opened');
+    toggleClassName(menu, MAIN_MENU_OPENED_CLASSNAME);
   });
 
   menuOverlay.addEventListener('click', function() {
-    toggleClassName(menu, 'main__menu_opened');
+    toggleClassName(menu, MAIN_MENU_OPENED_CLASSNAME);
   });
 }
 
 function initPhotos() {
-  var photoOpeners = document.querySelectorAll('[data-photo-opener]');
+  var photoOpeners = document.querySelectorAll(PHOTO_OPENER_SELECTOR);
   
   var openerCounter = 0;
   var opener;
   var photoOverlay;
   
   while (opener = photoOpeners[openerCounter]) {
-    photoOverlay = opener.parentNode.querySelector('[data-photo-overlay]');
-    photoCloser = photoOverlay.querySelector('[data-photo-closer]');
+    photoOverlay = opener.parentNode.querySelector(PHOTO_OVERLAY_SELECTOR);
+    photoCloser = photoOverlay.querySelector(PHOTO_CLOSER_SELECTOR);
     
     opener.addEventListener('click', function(event) {
       var photoViewParent = event.target.nodeName === 'IMG'
         ? event.target.parentNode.parentNode
         : event.target.parentNode;
 
-      var photoView = photoViewParent.querySelector('[data-photo-view]')
+      var photoView = photoViewParent.querySelector(PHOTO_VIEW_SELECTOR)
 
-      toggleClassName(photoView, 'photo-view_opened');
+      toggleClassName(photoView, PHOTO_VIEW_OPENED_CLASSNAME);
     });
 
     photoOverlay.addEventListener('click', function(event) {
       var photoView = event.target.parentNode;
 
-      toggleClassName(photoView, 'photo-view_opened');
+      toggleClassName(photoView, PHOTO_VIEW_OPENED_CLASSNAME);
     });
 
     photoCloser.addEventListener('click', function(event) {
       var photoView = event.target.parentNode.parentNode;
 
-      toggleClassName(photoView, 'photo-view_opened');
+      toggleClassName(photoView, PHOTO_VIEW_OPENED_CLASSNAME);
     });
 
     openerCounter += 1;
